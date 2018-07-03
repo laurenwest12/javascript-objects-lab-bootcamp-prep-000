62f2ef9fc9c441a5ae650aabe03741fbde8ad14c
Lauren West <laurenwest12@gmail.com>
2018-07-03 21:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var recipes = {};
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({},obj,{[key]:value});
   }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
+  obj[key] = value
+  return obj
+}
